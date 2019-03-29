0d49c650cce7c9c7a513c1cc9c9e48f24e4a832c
stevenadiazr <36484829+stevenadiazr@users.noreply.github.com>
2019-03-29 00:07:15 -0500

    arreglos2.0

--- Restaurante/ src/ restaurante/Mesa.java
+++ Restaurante/ src/ restaurante/Mesa.java
@@ -1,41 +1,56 @@
-package restaurantepackage restaurante;
+package restaurante;
 
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Menu {
-    public static void Inicio(){
-        ArrayList<ArrayList> listaMesa = new ArrayList<>();
-        menu(listaMesa);
-    }
+public class Mesa {
     
-    public static void menu(ArrayList<ArrayList> listaMesa){
+    public static void AgregarMesa(ArrayList<ArrayList> listaMesa){
+        
         Scanner in = new Scanner(System.in);
+        ArrayList<Persona> personasMesa;
+        int numMesa, cantPersonas;
         
-        System.out.println("Ingrese la acción a realizar");
-        System.out.println("Presione 1 para agregar mesa");
-        System.out.println("Presione 2 para agregar persona");
-        System.out.println("Presione 3 para agregar orden");
-        System.out.println("Presione 4 para mostrar la cuenta");
-        System.out.println("Presione 5 para pagar la cuenta");
-        int opcion = in.nextInt();
+        System.out.println("Ingrese numero de mesa");
+        numMesa = in.nextInt();
+        System.out.println("Ingrese cantidad de personas");
+        cantPersonas = in.nextInt();
         
-        switch(opcion){
-            case 1:
-                Mesa.AgregarMesa(listaMesa);
-            case 2:
-                Mesa.AgregarMesa(listaMesa);
-            case 3:
-                Mesa.AgregarMesa(listaMesa);
-            case 4:
-                Mesa.AgregarMesa(listaMesa);
-            case 5:
-                Mesa.AgregarMesa(listaMesa);
-            default:{
-                System.out.println("ERROR");
-                System.out.println("Seleccione una opción válida");
-            }
-                
-        }
+        personasMesa = new ArrayList<>(cantPersonas);
+        
+        AgregarPersona(listaMesa, cantPersonas);
+    }
+
+    public static void AgregarPersona(ArrayList<ArrayList> listaMesa, int cantPersonas){
+        Persona persona = new Persona();
+        Scanner in = new Scanner(System.in);
+        ArrayList<Persona> personasMesa;
+        personasMesa = new ArrayList<>(cantPersonas);
+        int numMesa;
+        
+        for (int i = 0; i < cantPersonas; i++){
+            System.out.println("Ingrese nombre de la persona");
+            persona.setNombre(in.nextLine());
+            System.out.println("Ingrese edad de la persona");
+            persona.setEdad(in.nextInt()); in.nextLine();
+            System.out.println("Ingrese forma de pago de la persona (0 para efectivo, 1 para tarjeta)");
+            persona.setFormaDePago(in.nextInt()); in.nextLine();
+            
+            if (persona.getFormaDePago() == 1){
+                System.out.println("Ingrese número de la tarjeta (8 números)");
+                int[] nTarjeta = new int[8];
+                for (int j = 0; j < 8; j++ ){
+                    System.out.println("Ingrese el valor " + (j+1) + " de la tarjeta");
+                    nTarjeta[j] = in.nextByte(); in.nextLine();
+                }
+                System.out.println("Se pagará con tarjeta");
+                persona.setNumeroTarjeta(nTarjeta);
+            } else
+                System.out.println("Se pagará con efectivo");
+            persona.setPersona(persona);
+            personasMesa.add(persona.getPersona());
+        }   
+        listaMesa.add(personasMesa);
+        System.out.println(listaMesa.get(0));
     }
 }
